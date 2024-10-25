252eb05640d929f6ba60118e74c7e12cdfa51c9f
peterc-s <petercs@skiff.com>
2024-10-25 21:37:24 +0100

    setup build.zig to build C

---
+++ build.zig
@@ -0,0 +1,30 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    // create executable
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "tereminder",
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // add source files
+    exe.addCSourceFile(.{ .file = b.path("src/main.c") });
+
+    // add include paths
+    exe.addIncludePath(b.path("src"));
+
+    // link against any necessary C libraries
+    exe.linkSystemLibrary("c");
+
+    // build the executable
+    b.installArtifact(exe);
+
+    // create run option
+    const run_exe = b.addRunArtifact(exe);
+    const run_step = b.step("run", "Run the program.");
+    run_step.dependOn(&run_exe.step);
+}
