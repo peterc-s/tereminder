720e956939759510b66ec652ccda52281569514c
peterc-s <petercs@skiff.com>
2024-11-02 10:58:51 +0000

    remove commented code and free reminders

--- src/main.c
+++ src/main.c
@@ -6,29 +6,21 @@
 #include "file.h"
 
 int main(void) {
-    // struct tm test_time = {
-    //     .tm_year = 124,
-    //     .tm_mon  = 10,
-    //     .tm_mday = 25,
-    //     .tm_hour = 22,
-    // };
-    //
-    // reminder_t reminder = {
-    //     .due = test_time,
-    //     .severity = High,
-    //     .title = "Hello",
-    //     .description = "World"
-    // };
-    //
-    // print_reminder(&reminder);
-
     char* reminder_file = read_file("reminders.trdr");
 
     reminder_arr_t reminders = parse_file(reminder_file);
 
+    // free reminder file now that we don't need it
     free(reminder_file);
 
     for (size_t i = 0; i < reminders.size; ++i) {
         print_reminder(&reminders.arr[i]);
     }
+
+    // finally, free all reminder memory
+    for(size_t i = 0; i < reminders.size; ++i) {
+        free(reminders.arr[i].title);
+        free(reminders.arr[i].description);
+    }
+    free(reminders.arr);
 }
